e3b4409189cb4cb30084044fd75bf4fadae62ef0
nextor <nextor@gmail.com>
2025-05-03 10:37:40 +0000

    feat: set list products as main route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
+import { ListProductsComponent } from './pages/list-products/list-products.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: ListProductsComponent,
+  },
 ];
